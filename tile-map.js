6bcd7cceafb4c047ee905fe3fa5fa4b4b46ed755
tattii <yuta.tatti@gmail.com>
2018-05-13 08:41:48 +0000

    set config

--- tile-map.js
+++ tile-map.js
@@ -2,18 +2,22 @@ var tileReduce = require('@mapbox/tile-reduce');
 var path = require('path');
 var fs = require('fs');
 
+var config = {
+  mbtiles: 'tiles.mbtiles',
+  minzoom: 0,
+  maxzoom: 14,
+  style: 'http://172.17.0.1:8080/styles/klokantech-basic/style.json',
+  dir: 'tile2',
+  retina: true
+};
+
+
 tileReduce({
-  zoom: 14,
+  zoom: 5,
   map: path.join(__dirname, '/tile-worker.js'),
-  sourceCover: 'osm',
-  sources: [{name: 'osm', mbtiles: path.join(__dirname, 'tiles.mbtiles')}],
-  output: fs.createWriteStream('tile-map.log'),
-  log: true,
-  mapOptions: {
-    style: 'http://172.17.0.1:8080/styles/klokantech-basic/style.json',
-    dir: 'tile2',
-    retina: true
-  }
+  sourceCover: config.mbtiles,
+  sources: [{name: config.mbtiles, mbtiles: path.join(__dirname, config.mbtiles)}],
+  mapOptions: config
 })
 .on('end', function() {
   console.log('done');
